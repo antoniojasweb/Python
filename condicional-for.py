c3e0d0a08a8f0383cd5eb1c3ed7fb23648bb8d0f
antoniojasweb <65303510+antoniojasweb@users.noreply.github.com>
2025-02-12 16:53:23 +0100

    Delete condicional-for.py

--- condicional-for.py
+++
@@ -1,9 +0,0 @@
-for valor in range(10):
-    print("El valor es:", valor)
-
-# Lista de números
-numeros = [1, 2, 3, 4, 5]
-
-# Bucle for para iterar sobre la lista de números
-for numero in numeros:
-    print(f"El número es: {numero}")
